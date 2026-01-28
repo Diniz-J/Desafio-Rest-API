c326fe12917afad1f292adaa83b29ecd49355b95
DinizJ <rodrigocljr07@gmail.com>
2026-01-28 15:01:07 -0300

    Saving new tasks

--- internal/repository/task_repository.go
+++ internal/repository/task_repository.go
@@ -1,5 +1,42 @@
 package repository
 
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/DinizJ/desafio/internal/model"
+)
+
 //Queries SQL, acesso a banco
 
 // DB
+
+type TaskRepository struct {
+	db *sql.DB // connect
+}
+
+// Save put new task
+func (r *TaskRepository) Save(ctx context.Context, task *model.Task) error {
+	query := `
+		INSERT INTO tasks (id, title, description, status, priority, created_at, updated_at, deleted_at)
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?)	    
+    `
+
+	_, err := r.db.ExecContext(ctx, query,
+		task.ID,
+		task.Title,
+		task.Description,
+		task.Status,
+		task.Priority,
+		task.CreatedAt,
+		task.UpdatedAt,
+		task.DeletedAt,
+	)
+
+	if err != nil {
+		return fmt.Errorf("falha ao salvar task no banco:%w", err)
+	}
+
+	return nil
+}
