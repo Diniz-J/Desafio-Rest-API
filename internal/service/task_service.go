c853ca3672aea841080ad3a5d95d8bbf70dc276b
DinizJ <rodrigocljr07@gmail.com>
2026-01-28 14:13:34 -0300

    begin service

--- internal/service/task_service.go
+++ internal/service/task_service.go
@@ -1,5 +1,32 @@
 package service
 
+import (
+	"fmt"
+
+	"github.com/DinizJ/desafio/task"
+	"github.com/google/uuid"
+)
+
 // Lógica de negócio, validações, chamadas ao repository
 
 // Validate
+
+type TaskService struct {
+	//repository
+}
+
+func (s *TaskService) CreateId() (string, error) {
+
+	id := uuid.New().String()
+
+	t := &Task{
+		ID: id,
+	}
+	if err := s.repo.Save(t); err != nil {
+		return "", fmt.Errorf("Cannot create task: %w", err)
+	}
+
+	return id, nil
+}
+
+func (s *TaskService) CreateTitle()
