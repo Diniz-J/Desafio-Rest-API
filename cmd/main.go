fbaf1beb1ec48b564b07c3615336cb1b57b4df44
DinizJ <rodrigocljr07@gmail.com>
2026-01-29 14:33:32 -0300

    Adjust Branch Structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,72 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+package main
+
+import (
+"database/sql"
+
+_ "github.com/go-sql-driver/mysql"
+
+"log"
+"net/http"
+"os"
+
+"github.com/DinizJ/desafio/internal/handler"
+"github.com/DinizJ/desafio/internal/repository"
+"github.com/DinizJ/desafio/internal/service"
+)
+
 func main() {
 
-	server := gin.Default()
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
 
-	server.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"msg": "pong",
-		})
-	})
+	dsn := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName
+	log.Printf("Conectando em: %s", dsn)
 
-	if err := server.Run(":8080"); err != nil {
-		log.Fatal(err)
+	//conectar ao banco
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalf("Erro ao conectar: %s", err)
+	}
+	defer db.Close()
+
+	//Ping
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Erro ao fazer ping: %v", err)
+	}
+	log.Printf("Conectado com sucesso")
+
+	//Inicializa as layers
+	repo := repository.NewTaskRepository(db)
+	svc := service.NewTaskService(repo)
+	hdl := handler.NewTaskHandler(svc)
+
+	//Config das rotas
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /api/v1/tasks", hdl.CreateTask)
+	mux.HandleFunc("GET /api/v1/tasks", hdl.ListTask)
+	mux.HandleFunc("GET /api/v1/tasks/{id}", hdl.GetTask)
+	mux.HandleFunc("PUT /api/v1/tasks/{id}", hdl.UpdateTask)
+	mux.HandleFunc("DELETE /api/v1/tasks/{id}", hdl.DeleteTask)
+	mux.HandleFunc("PATCH /api/v1/tasks/{id}/complete", hdl.CompleteTask)
+
+	// Roda servidor
+	log.Println("Servidor rodando em :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("Erro ao rodar servidor: %v", err)
 	}
 }
+
