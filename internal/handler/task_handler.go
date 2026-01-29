2012c57d9874bc97baa1ba0801f1ed59809ec21d
DinizJ <rodrigocljr07@gmail.com>
2026-01-29 11:08:54 -0300

    implement task handler

--- internal/handler/task_handler.go
+++ internal/handler/task_handler.go
@@ -1,5 +1,52 @@
 package handler
 
-// receber request, validar sintaxe, chamar service, retornar response
+import (
+	"encoding/json"
+	"net/http"
 
-// HTTP
+	"github.com/DinizJ/desafio/internal/service"
+)
+
+type TaskHandler struct {
+	service *service.TaskService
+}
+
+// Cria nova tafera
+
+func (h *TaskHandler) CreateTask(w http.ResponseWriter, r *http.Request) {
+
+	//Parse request
+	var req struct {
+		Title       string `json:"title"`
+		Description string `json:"description"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid request format", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+
+	//Validar entrada
+	if req.Title == "" {
+		http.Error(w, "title is required", http.StatusBadRequest)
+		return
+	}
+
+	//Chama service
+	task, err := h.service.CreateTask(r.Context(), req.Title, req.Description)
+	//r.Context() é cancelado se o cliente fechar a conexão ou der timeout!
+	if err != nil {
+		//erro em service
+		http.Error(w, "failed to create task", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	//Erro ja tratado!
+	if err := json.NewEncoder(w).Encode(task); err != nil {
+		http.Error(w, "failed to encode response", http.StatusInternalServerError)
+	}
+}
