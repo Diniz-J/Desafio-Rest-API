16a1291259216bb5498cbca6f8b179fc0a0eec04
DinizJ <rodrigocljr07@gmail.com>
2026-01-29 13:02:12 -0300

    Adjust settings

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/DinizJ/desafio/internal/handler"
+	"github.com/DinizJ/desafio/internal/repository"
+	"github.com/DinizJ/desafio/internal/service"
 )
 
 func main() {
@@ -18,7 +26,7 @@ func main() {
 	log.Printf("Conectando em: %s", dsn)
 
 	//conectar ao banco
-	db err := sql.Open("mysql", dsn)
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("Erro ao conectar: %s", err)
 	}
@@ -35,6 +43,18 @@ func main() {
 	svc := service.NewTaskService(repo)
 	hdl := handler.NewTaskHandler(svc)
 
+	//Config das rotas
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /api/v1/tasks", hdl.CreateTask)
+	mux.HandleFunc("GET /api/v1/tasks", hdl.ListTask)
+	mux.HandleFunc("GET /api/v1/tasks/{id}", hdl.GetTask)
+	mux.HandleFunc("PUT /api/v1/tasks/{id}", hdl.UpdateTask)
+	mux.HandleFunc("DELETE /api/v1/tasks/{id}", hdl.DeleteTask)
+	mux.HandleFunc("PATCH /api/v1/tasks/{id}/complete", hdl.CompleteTask)
 
-
+	// Roda servidor
+	log.Println("Servidor rodando em :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("Erro ao rodar servidor: %v", err)
+	}
 }
